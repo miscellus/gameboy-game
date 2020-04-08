afe3d9d1518876fc7970bbe6745d0fbc19e08344
Jakob Kjær-Kammersgaard <jakob@miscellus.dk>
2020-04-08 13:46:07 +0200

    rename

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rgbasm -o main.obj main.gbz80 && \
+rgbasm -o main.obj main.z80 && \
 rgblink -m main.map -n main.sym -o main.gb main.obj && \
 rgbfix -p 0 -v main.gb && \
 STEAM_COMPAT_DATA_PATH=~/.proton/ ~/.steam/steam/steamapps/common/Proton*/proton run ../BGB/bgb.exe main.gb
