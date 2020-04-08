d21ac09f4de9fe341ac766781af9707739be3775
Jakob Kjær-Kammersgaard <jakob@miscellus.dk>
2020-04-08 13:36:29 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rgbasm -o main.obj main.gbz80 && \
+rgblink -m main.map -n main.sym -o main.gb main.obj && \
+rgbfix -p 0 -v main.gb && \
+STEAM_COMPAT_DATA_PATH=~/.proton/ ~/.steam/steam/steamapps/common/Proton*/proton run ../BGB/bgb.exe main.gb
