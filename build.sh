9ae1d62a3ac642efe66cde11e17c25417c6440f0
Jakob Kjær-Kammersgaard <jakob@miscellus.dk>
2020-04-10 23:29:08 +0200

    Movement tweaks

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 rgbasm -o main.obj main.z80 && \
 rgblink -m main.map -n main.sym -o main.gb main.obj && \
 rgbfix -p 0 -v main.gb && \
+#mednafen main.gb
 STEAM_COMPAT_DATA_PATH=~/.proton/ ~/.steam/steam/steamapps/common/Proton*/proton run ../BGB/bgb.exe main.gb
