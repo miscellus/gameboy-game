09b78c0cdda25eafe5025d17c2c4e27eccecc4a6
Jakob Kjær-Kammersgaard <jakob@miscellus.com>
2025-08-04 23:34:01 +0200

    show tile and pixel grids at different zoom-levels

--- tools/world_edit/world_edit.c
+++ tools/world_edit/world_edit.c
@@ -3,15 +3,24 @@
 #include <raymath.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #define WE_COLOR_GRAY_LIGHT ((Color){192, 192, 192, 255})
-#define ZOOM_MIN 0.05f
-#define ZOOM_MAX 50.0f
-#define ZOOM_SHOW_LINES_THRESHOLD 10.0f
+#define ZOOM_MIN 1.0f
+#define ZOOM_MAX 60.0f
+#define ZOOM_SHOW_PIXELS 10.0f
+#define ZOOM_SHOW_TILES 1.0f
+
+#define COLOR_GB_OFF ((Color){194, 207, 168, 255})
+#define COLOR_GB_LIGHT ((Color){173, 191, 146, 255})
+#define COLOR_GB_MID_LIGHT ((Color){150, 166, 124, 255})
+#define COLOR_GB_MID_DARK ((Color){114, 126, 100, 255})
+#define COLOR_GB_DARK ((Color){90, 99, 92, 255})
 
 typedef struct Tile
 {
     Texture2D *texture;
+    uint8_t color_indexes[8*8]; // NOTE(jkk): In range 0-3
     bool solid;
 } Tile;
 
@@ -54,6 +63,7 @@ int main(int argc, char **argv)
 
     InitWindow(800, 600, "Hello, Raylib!");
     SetWindowState(FLAG_WINDOW_RESIZABLE);
+    SetTargetFPS(60);
 
     dpi = GetWindowScaleDPI();
 
@@ -62,7 +72,16 @@ int main(int argc, char **argv)
     level.height = 64;
     level.tiles = calloc(level.width * level.height, sizeof(*level.tiles));
 
-    Image test_image = GenImageGradientRadial(8, 8, 1.0f, WHITE, BLACK);
+    Color gb_colors_0[] =
+    {
+        COLOR_GB_OFF,
+        COLOR_GB_LIGHT,
+        COLOR_GB_MID_LIGHT,
+        COLOR_GB_MID_DARK,
+        COLOR_GB_DARK,
+    };
+
+    Image test_image = GenImageColor(8, 8, gb_colors_0[1]);
     Texture2D test_texture = LoadTextureFromImage(test_image);
 
     for (size_t y = 0; y < level.height; ++y)
@@ -82,11 +101,10 @@ int main(int argc, char **argv)
     while (!WindowShouldClose())
     {
         Vector2 mouse_pos_screen = GetMousePosition();
+        Vector2 mouse_pos_world = GetScreenToWorld2D(mouse_pos_screen, camera);
         float mouse_scroll = GetMouseWheelMoveV().y;
 
         CameraUpdateZoom(&camera, mouse_scroll, mouse_pos_screen, ZOOM_MIN, ZOOM_MAX);
-
-
 
         BeginDrawing();
         BeginMode2D(camera);
@@ -100,33 +118,64 @@ int main(int argc, char **argv)
             }
         }
 
-        if (camera.zoom > ZOOM_SHOW_LINES_THRESHOLD)
-        {
-            Vector2 world_view_min = (Vector2){0, 0};
-            Vector2 world_view_max = (Vector2){level.width * 8.0f, level.height * 8.0f};
-            Color line_color = (Color){0, 0, 0, 48};
+        // if (camera.zoom > ZOOM_SHOW_LINES_THRESHOLD)
+        // {
+        //     Vector2 world_view_min = (Vector2){0, 0};
+        //     Vector2 world_view_max = (Vector2){level.width * 8.0f, level.height * 8.0f};
+        //     Color line_color = (Color){0, 0, 0, 48};
+        //
+        //     for (float y = world_view_min.y; y < world_view_max.y; y += 1.0f)
+        //     {
+        //         Vector2 start_pos = {world_view_min.x, y};
+        //         Vector2 end_pos = {world_view_max.x, y};
+        //         DrawLineEx(start_pos, end_pos, 0.1f, line_color);
+        //     }
+        //
+        //     for (float x = world_view_min.x; x < world_view_max.x; x += 1.0f)
+        //     {
+        //         Vector2 start_pos = {x, world_view_min.y};
+        //         Vector2 end_pos = {x, world_view_max.y};
+        //         DrawLineEx(start_pos, end_pos, 0.1f, line_color);
+        //     }
+        // }
 
-            for (float y = world_view_min.y; y < world_view_max.y; y += 1.0f)
-            {
-                Vector2 start_pos = {world_view_min.x, y};
-                Vector2 end_pos = {world_view_max.x, y};
-                DrawLineV(start_pos, end_pos, line_color);
-            }
-
-            for (float x = world_view_min.x; x < world_view_max.x; x += 1.0f)
-            {
-                Vector2 start_pos = {x, world_view_min.y};
-                Vector2 end_pos = {x, world_view_max.y};
-                DrawLineV(start_pos, end_pos, line_color);
-            }
-        }
+        Vector2 pixel_rect_min = (Vector2) {floorf(mouse_pos_world.x), floorf(mouse_pos_world.y)};
+        DrawRectangleV(pixel_rect_min, (Vector2){1, 1}, (Color){255, 255, 0, 192});
 
         EndMode2D();
 
-        DrawRectangle((int)mouse_pos_screen.x - 10, (int)mouse_pos_screen.y - 10, 20, 20, BLACK);
+        if (camera.zoom > ZOOM_SHOW_TILES)
+        {
+            Vector2 grid_start = GetWorldToScreen2D((Vector2){0,0}, camera);
+            Vector2 grid_end = GetWorldToScreen2D((Vector2){level.width * 8.0f, level.height * 8.0f}, camera);
 
-        DrawText(TextFormat("DPI: (%g, %g)", dpi.x, dpi.y), 20, 20, (int)(24 * dpi.y), RED);
-        EndDrawing();
+            Color line_color = (Color){0, 0, 0, 48};
+
+            // for (float y = grid_start.y; y < grid_end.y; y += screen_pixels_per_world_pixel)
+            for (int y = 0; y < level.height * 8; ++y)
+            {
+                float yf = grid_start.y + y * camera.zoom;
+                Vector2 start_pos = {grid_start.x, yf};
+                Vector2 end_pos = {grid_end.x, yf};
+                bool on_tile_boundary = (y & 0x7) == 0;
+                if (on_tile_boundary || (y & 7) && camera.zoom > ZOOM_SHOW_PIXELS) {
+                    float line_width = on_tile_boundary ? 3.0f : 1.0f;
+                    DrawLineEx(start_pos, end_pos, line_width, line_color);
+                }
+            }
+
+            for (int x = 0; x < level.width * 8; ++x)
+            {
+                float xf = grid_start.x + x * camera.zoom;
+                Vector2 start_pos = {xf, grid_start.y};
+                Vector2 end_pos = {xf, grid_end.y};
+                bool on_tile_boundary = (x & 0x7) == 0;
+                if (on_tile_boundary || (x & 7) && camera.zoom > ZOOM_SHOW_PIXELS) {
+                    float line_width = on_tile_boundary ? 3.0f : 1.0f;
+                    DrawLineEx(start_pos, end_pos, line_width, line_color);
+                }
+            }
+        }        EndDrawing();
     }
     return 0;
 }
